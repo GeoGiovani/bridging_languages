facbca9d2037f4c13e4fe074e657facb27bb8819
George Giovanis <georgedgiovani.dev@gmail.com>
2020-01-25 14:16:33 -0800

    Add: Finished Q2
    Signed-off-by: George Giovanis <georgedgiovani.dev@gmail.com>

--- Q2/caseparse.py
+++ Q2/caseparse.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
-import sys, re
+import sys, re, os
 
 args = sys.argv
 case_name = args[args.index("-case")+1]
+
+
 
 f = sys.stdin.readline().rstrip()
 new = []
@@ -22,3 +24,7 @@ if "_" in f:
     f = f.replace("_", " ").lower()
 elif "-" in f:
     f = f.replace("-", " ").lower()
+
+# print(case_name + " " + f)
+os.system("g++ output.cpp")
+os.system("./a.out " + case_name + " " + f)
