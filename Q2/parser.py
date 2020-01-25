d203d866c5eb2238c6b4a07fbbb81e66028e1a43
George Giovanis <georgedgiovani.dev@gmail.com>
2020-01-25 11:33:23 -0800

    Update: Python parser
    Signed-off-by: George Giovanis <georgedgiovani.dev@gmail.com>

--- Q2/parser.py
+++ Q2/parser.py
@@ -12,15 +12,32 @@ def parse(input, type):
     elif (type == "camelCase"):
         for c in input:
             if c.isupper():
+                words.insert(word)
+                word = ""
                 word += c
-                words.inser(word)
             else:
                 word += c
 
     elif (type == "PascalCase"):
-        
+        #Counter to ensure first word doesnt cause bug
+        i = 0
+        for c in input:
+            if c.isupper() and i != 0:
+                words.insert(word)
+                word = ""
+                word += c
+            else: 
+                word += c
+        i += 1
 
     elif (type == "kebab-case"):
+        for c in input:
+            if c == '_':
+                words.insert(word)
+                word = ""
+            else:
+                word += c
 
+    #Add last word
+    words.insert(word)
     return words
-    
